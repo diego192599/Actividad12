1667cb977e59e18de3fb1bfc2d59cf90a09a70f7
diego192599 <di.cramire@udeo.edu.gt>
2025-08-06 10:45:58 -0600

    diccionario creado y creacion de metodo ingreso

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("MEnu")
+repartidor={}
+def Ingreso():
+    cantidad=int(input("Ingrese la cantidad de repartidores que participaron: "))
+    for i in range(cantidad):
+        nombre=input("Ingrese el nombre del repartidor: ")
+        paquetes=
